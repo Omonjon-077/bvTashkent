96b7ad5a6610813ae095b1fbae542346decfad7b
Omonjon <omonjons077@gmail.com>
2023-12-08 18:21:23 +0500

    Bags fixed

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 300 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 300 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== Header Fixed ===============*/
+if ($("#myHeader").length) {
+    window.onscroll = function () {
+        myFunction()
+    };
+
+    let header = document.getElementById("myHeader");
+    let sticky = header.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
